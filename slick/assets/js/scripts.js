a0e35added10eaa9d85cc8b31e4fc8670b98a316
basshitt84 <157418893+basshitt84@users.noreply.github.com>
2024-05-01 21:56:36 +0000

    slideshow

---
+++ slick/assets/js/scripts.js
@@ -0,0 +1,21 @@
+
+
+$(document).ready(function() {
+
+    $('.fade').slick({
+      dots: false,
+      infinite: true,
+      speed: 800,
+      fade: false,
+      slide: 'div',
+      cssEase: 'linear',
+      autoplay: true,
+      autoplaySpeed: 20
+      
+
+      
+    });
+
+    
+
+});

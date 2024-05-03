51287b1d895ac19a02265ef34f67695f9f3559d2
basshitt84 <157418893+basshitt84@users.noreply.github.com>
2024-05-03 01:03:34 +0000

    slideshow

---
+++ spacex-final/assets/js/scripts.js
@@ -0,0 +1,21 @@
+
+
+$(document).ready(function() {
+
+    $('.fade').slick({
+      dots: false,
+      infinite: true,
+      speed: 8500,
+      fade: false,
+      slide: 'div',
+      cssEase: 'linear',
+      autoplay: true,
+      autoplaySpeed: 20
+      
+
+      
+    });
+
+    
+
+});
